f29af4c9d940b9b0bdf4364677b6c960e91c327d
sefa-murat <muratcetinerxd@gmail.com>
2018-03-14 17:42:31 +0300

    Simple utility files for mongodb and twitter api

--- tweetcollector.py
+++ tweetcollector.py
@@ -19,8 +19,8 @@ class TwitterAPI:
 
     def get_user_tweets(self, screen_name):
         for tweet in tweepy.Cursor(self.api.user_timeline, screen_name=screen_name, tweet_mode="extended").items():
-            yield {'text': tweet.full_text}
+            yield {'text': tweet.full_text, 'label': ''}
 
     def search(self, search_query):
         for tweet in tweepy.Cursor(self.api.search, q=search_query, tweet_mode="extended").items():
-            yield {'text': tweet.full_text}
+            yield {'text': tweet.full_text, 'label': ''}
