c1bcbc56e1019f472dd7330e74850f46cc94231e
MuratCetiner <30980747+MuratCetiner@users.noreply.github.com>
2018-03-16 22:55:23 +0300

    Add files via upload

--- tweet_labeler/tweets2json.py
+++ tweet_labeler/tweets2json.py
@@ -11,10 +11,10 @@ def tw2js(data):
 
 
 # Twitter API
-consumer_key = 'uzludXf9EsGOGfoPpL4ibe1N6'
-consumer_secret = 'QABjGRsJ46N7KFPRAHyy3Fl8S8x2OnrnvkeORK4pedXRFNviv1'
-access_token = '703245298932563968-LCnaCNU8qNkqoHpFabWH1LWt1Qaay9I'
-access_token_secret = 'qXPH1ES9EvdwPi2JAwZAjKHENyTtoPO9swWybJi7mvp8K'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # Initializes your TwitterAPI object
 tw = TwitterAPI(consumer_key, consumer_secret, access_token, access_token_secret)
